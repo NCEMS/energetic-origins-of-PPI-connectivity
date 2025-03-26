fa579c3a3e4edd95f26e5c01fee9eb9c5f1bc21c
Dan Nissley <dan182@psu.edu>
2025-03-26 17:18:46 +0000

    Update snakefile

--- bash-scripts/env-setup.sh
+++ bash-scripts/env-setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 conda create -n snakemake -c bioconda -y snakemake
 conda env create --name network-analysis --file=network-analysis.yml
 conda env create --name cagiada-stability --file=cagiada-stability.yml
