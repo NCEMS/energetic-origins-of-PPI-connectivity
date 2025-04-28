531ce2c785349ad969d34656861d578b1cef4000
Dan Nissley <dan182@psu.edu>
2025-04-28 15:15:17 +0000

    various updates

--- 6-protein-half-life/python-scripts/add-half-life.py
+++ 6-protein-half-life/python-scripts/add-half-life.py
@@ -42,8 +42,10 @@ def main():
         "--organism_tag",
         help="Organism label for this run")
     parser.add_argument("--halflife_db")
-    parser.add_argument("--run_cagiada", type=bool)
+    parser.add_argument("--run_cagiada", type=str)
     args = parser.parse_args()
+
+    run_cagiada = args.run_cagiada.lower() == "true"
 
     nodes_df = pd.read_pickle(args.nodes)
 
@@ -57,10 +59,10 @@ def main():
 
     nodes_df = add_halflife(nodes_df, halflife_df, merge_col1, merge_col2, halflife_df_cols)
 
-    if args.run_cagiada:
-        nodes_df.to_pickle(f"{args.output_dir}-{args.output_prefix}-{args.organism_tag}-nodes-centrality-seqs-DeepTMHMM-SignalP-UniProt-IDRs-albatross-cider-GhoshDill-Cagiada-halflife.pkl")
+    if run_cagiada:
+        nodes_df.to_pickle(f"{args.output_dir}/{args.output_prefix}-{args.organism_tag}-nodes-centrality-seqs-DeepTMHMM-SignalP-UniProt-IDRs-albatross-cider-GhoshDill-Cagiada-halflife.pkl")
     else:
-        nodes_df.to_pickle(f"{args.output_dir}-{args.output_prefix}-{args.organism_tag}-nodes-centrality-seqs-DeepTMHMM-SignalP-UniProt-IDRs-albatross-cider-GhoshDill-halflife.pkl")
+        nodes_df.to_pickle(f"{args.output_dir}/{args.output_prefix}-{args.organism_tag}-nodes-centrality-seqs-DeepTMHMM-SignalP-UniProt-IDRs-albatross-cider-GhoshDill-halflife.pkl")
 
 if __name__ == "__main__":
     main()
