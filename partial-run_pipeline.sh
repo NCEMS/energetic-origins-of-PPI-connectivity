4af27da17305eca6615f6065ed64c12f44bddef5
Dan Nissley <dan182@psu.edu>
2025-05-01 12:23:55 -0700

    translation speed additions

--- partial-run_pipeline.sh
+++ partial-run_pipeline.sh
@@ -8,13 +8,13 @@ CONFIG_FILE=$1
 #STAGES=("3-uniprot-annotation")
 #STAGES=("4-idr-properties")
 #STAGES=("5-dG-calculations")
-#STAGES=("6-Rosetta-scoring")
+STAGES=("6-Rosetta-scoring")
 #STAGES=("7-FoldX-scoring")
 #STAGES=("8-protein-half-life")
 #STAGES=("9-protein-expression")
 #STAGES=("flatten")
 
-STAGES=("6-Rosetta-scoring" "7-FoldX-scoring" "8-protein-half-life" "9-protein-expression" "flatten")
+#STAGES=("6-Rosetta-scoring" "7-FoldX-scoring" "8-protein-half-life" "9-protein-expression" "flatten")
 
 for STAGE in "${STAGES[@]}"; do
     echo -e "\nRunning stage $STAGE with config $CONFIG_FILE"
