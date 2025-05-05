4671371d9d872c4df95e103c8cd73970cfb0f43a
Dan Nissley <dan182@psu.edu>
2025-05-05 13:42:59 -0400

    Minor updates

--- partial-run_pipeline.sh
+++ partial-run_pipeline.sh
@@ -8,11 +8,11 @@ CONFIG_FILE=$1
 #STAGES=("3-uniprot-annotation")
 #STAGES=("4-idr-properties")
 #STAGES=("5-dG-calculations")
-STAGES=("6-Rosetta-scoring")
+#STAGES=("6-Rosetta-scoring")
 #STAGES=("7-FoldX-scoring")
 #STAGES=("8-protein-half-life")
 #STAGES=("9-protein-expression")
-#STAGES=("flatten")
+STAGES=("flatten")
 
 #STAGES=("6-Rosetta-scoring" "7-FoldX-scoring" "8-protein-half-life" "9-protein-expression" "flatten")
 
