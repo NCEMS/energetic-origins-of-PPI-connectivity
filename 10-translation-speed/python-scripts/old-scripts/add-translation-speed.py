736551bd19665ee75b074823db525ce86772f5ac
Dan Nissley <dan182@psu.edu>
2025-05-19 13:55:50 -0400

    Add translation efficiency info

--- 10-translation-speed/python-scripts/old-scripts/add-translation-speed.py
+++ 10-translation-speed/python-scripts/old-scripts/add-translation-speed.py
@@ -21,19 +21,19 @@ def main():
     parser.add_argument(
         "--organism_tag",
         help="Organism label for this run")
-    parser.add_argument("--input_file")
+    parser.add_argument("--trans_speed_file")
     args = parser.parse_args()
 
     # read in the nodes file
     nodes_df = pd.read_pickle(args.nodes)
+    #nodes_df = pd.read_csv(args.nodes)
 
-    # read in the translation efficiency information
-    TE_df = pd.read_csv(args.input_file, sep="\t")
+    # read in the translation speed information
+    trans_speed_df = pd.read_pickle(args.trans_speed_file)
 
     # merge the two pd.DataFrame objects to insert translation speed information into the nodes_df
-    nodes_df = nodes_df.merge(TE_df[["gene", "log2_TE"]], how="left", left_on="node", right_on="gene")
+    nodes_df = nodes_df.merge(trans_speed_df[["gene", "translation_speed_score"]], how="left", left_on="node", right_on="gene")
 
-    # save the resulting pd.DataFrame containing log2_TE information
     nodes_df.to_pickle(f"{args.output_dir}/{args.output_prefix}-{args.organism_tag}-nodes-centrality-seqs-DeepTMHMM-SignalP-UniProt-IDRs-albatross-cider-GhoshDill-Cagiada-Rosetta-FoldX-halflife-expr-speed.pkl")
 
 if __name__ == "__main__":
