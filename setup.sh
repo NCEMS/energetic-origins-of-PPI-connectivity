481a1e6209d8c344a5ab53ac971faedaf305c1b4
Dan Nissley <dan182@psu.edu>
2025-06-01 06:33:36 -0400

    Update notebook

--- setup.sh
+++ setup.sh
@@ -12,10 +12,10 @@ conda create -n snakemake-new -c conda-forge -c bioconda -c defaults snakemake=7
 # copy signalP to working directory
 #gocmd get --progress /iplant/home/shared/NCEMS/working-groups/energetic-origins/signalp6_fast 2-sequence-parsing/python-scripts/
 
-cp -r ../signalp6_fast 2-sequence-parsing/python-scripts/
+#cp -r ../signalp6_fast 2-sequence-parsing/python-scripts/
 
 # copy the signaP model files to the correct directory
-cp 2-sequence-parsing/python-scripts/signalp6_fast/signalp-6-package/models/distilled_model_signalp6.pt 2-sequence-parsing/python-scripts/signalp6_fast/signalp-6-package/signalp/model_weights/
+#cp 2-sequence-parsing/python-scripts/signalp6_fast/signalp-6-package/models/distilled_model_signalp6.pt 2-sequence-parsing/python-scripts/signalp6_fast/signalp-6-package/signalp/model_weights/
 
 # activate the snakemake pipeline
 #conda activate snakemake
